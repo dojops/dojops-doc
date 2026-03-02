a637f9137a8b7323e51d0f0cb5244f975d4f32b6
Mohamed Hedi CHLAGOU <chlagoumedhedi@outlook.com>
2026-03-02 23:58:40 +0100

    docs: Update readme file

--- content/_meta.js
+++ content/_meta.js
@@ -2,6 +2,7 @@ export default {
   index: "Introduction",
   "getting-started": "Getting Started",
   usage: "Usage",
+  tutorials: "Tutorials",
   architecture: "Architecture",
   components: "Components",
   community: "Community",
---
+++ content/tutorials/_meta.js
@@ -0,0 +1,5 @@
+export default {
+  "enhance-nextjs-project": "Enhance a Next.js Project",
+  "cicd-from-scratch": "CI/CD from Scratch",
+  "security-audit": "Security Audit & Remediation",
+};
