bb0bf53ab312b91133781ae71b8c5608a4a70ff3
Mohamed Hedi CHLAGOU <chlagoumedhedi@outlook.com>
2026-03-17 23:13:35 +0100

    feat: enhance security

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ export default withNextra({
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-ancestors 'none'",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com; connect-src 'self'; frame-ancestors 'none'",
           },
         ],
       },
